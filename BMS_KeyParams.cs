1b36c4ff1933232b7997a95a6df90f1fe4e92400
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2022-12-16 07:33:57 +0530

    BMS KeyParams are segregated
    instead of hardcoded strings

---
+++ BMS_KeyParams.cs
@@ -0,0 +1,12 @@
+﻿partial class BMS
+{
+    public class BMS_KeyParams
+    {
+        public static string Temp_string = "Temperature";
+        public static string SoC_string = "State of Charge";
+        public static string ChargeRate_string = "Charge Rate";
+
+    }
+}
+
+
