0ed575e46a58c89fbc2a7413ca6d176471326de7
MeenaMadhevanpillaiArunadevi <115541323+MeenaMadhevanpillaiArunadevi@users.noreply.github.com>
2023-01-03 23:09:01 +0530

    Extension

--- BMS_PrintConsole_ThresholdBreach.cs
+++ BMS_PrintConsole_ThresholdBreach.cs
@@ -1,15 +1,39 @@
-﻿using System;
+using BatteryManamgement;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static BatteryManamgement.BMS;
 
-partial class BMS
+namespace BatteryManamgement
 {
-    //Segregate IO if Threshold breached
-    static void printBMS_ThresholdBreach(string bmsCriteria)
+    partial class BMS
     {
-        Console.WriteLine(string.Concat(bmsCriteria, " is out of range"));
-    }
+        
+        static string generateBMS_printMessage(float value, float maxThreshold, float minThreshold, float warning_tolerance, BMS_KeyParams bmsCriteria, string userPreferredLanguage)
+        {
+           
+            LanguageCheck languageCheck = new ConcreteLanguageValidator();
+            ILanguage selectedLanguage = languageCheck.GetLanguage(userPreferredLanguage);
 
+
+            var bmsWarningMessage = selectedLanguage.isValueinLowBreachRange(value, minThreshold);
+            bmsWarningMessage = selectedLanguage.isValueinHighBreachRange(value, maxThreshold);
+            bmsWarningMessage = selectedLanguage.isValueinLowWarningRange(value, minThreshold, warning_tolerance);
+            bmsWarningMessage = selectedLanguage.isValueinHighWarningRange(value, maxThreshold, warning_tolerance);
+            bmsWarningMessage = selectedLanguage.isValueinNormalRange();
+
+
+            printBMS_ThresholdBreach(bmsWarningMessage, bmsCriteria);
+            return bmsWarningMessage;
+
+        }
+        static void printBMS_ThresholdBreach(string bmsWarningMessage, BMS_KeyParams bmsCriteria)
+        {
+            Console.WriteLine(string.Concat(bmsCriteria, " : ", bmsWarningMessage));           
+        }
+
+
+    }
 }
